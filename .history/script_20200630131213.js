4a75b8cb5e586727b2b920a0e76457c581eeb925
Elias Molato <ec.molato@gmail.com>
2020-07-08 11:20:29 -0700

    second

---
+++ .history/script_20200630131213.js
@@ -0,0 +1,7 @@
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('blist');
+const balance = document.getElementById('balance');
+const balance = document.getElementById('balance');
+const balance = document.getElementById('balance');
